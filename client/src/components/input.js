38c403225255bfabae54b7334ed5e18434cdb5cd
Hieu Truong <hieu.tnxd.bk@gmail.com>
2024-05-12 17:57:18 +0700

    chore: commit

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -1,15 +1,40 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 const reader = new FileReader()
 
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Image = styled.img`
+  width: 300px;
+  object-fit: cover;
+  border-radius: 10px;
+`
+
+const ImageContainer = styled.div`
+  margin: 10px;
+  text-align: start;
+  max-width: 300px;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+`
+const RecordNote = styled.p`
+  font-size: 16px;
+`
 const Input = ({}) => {
-  const [images, setImages] = useState([])
+  const [record, setRecords] = useState([])
   const getImage = async () => {
     try {
       const response = await fetch('http://localhost:5000/orders')
       const data = await response.json()
-      setImages(data)
+      const recordData = Object.keys(data).map((key) => data[key])
+      setRecords(recordData)
       return data
-      // Process the data and update the state or do something else with it
     } catch (error) {
       console.error('Error fetching image:', error)
       return null
@@ -49,16 +74,15 @@ const Input = ({}) => {
 
   return (
     <div>
-      <input
-        type='file'
-        placeholder='Enter your name'
-        id='file-upload'
-        multiple
-        onChange={(event) => uploadFileInput(event)}
-      />
-      {/* {images?.map((image) => (
-        <img key={image.id} src={image.src} alt='Dinosaur' />
-      ))} */}
+      <InputContainer>
+        {record?.map((record) => (
+          <ImageContainer key={record.id}>
+            <Image src={record.recipe.illustrate} alt='Dinosaur' />
+            <Title>{record.recipe.title}</Title>
+            <RecordNote>{record.recipe.notes}</RecordNote>
+          </ImageContainer>
+        ))}
+      </InputContainer>
     </div>
   )
 }
